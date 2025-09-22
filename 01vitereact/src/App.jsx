ce3aadd0fabe30129abb3ef618c47815c29c9577
prathmeshtiwar <prathmesh.211633101@vcet.edu.in>
2025-09-22 21:01:16 +0530

    DAY 1-REACT

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from './chai'
+function App() {
+
+  return (
+    <>
+      <Chai/>
+      <h1>hello chai </h1>
+      <p>hello code chai</p>
+    </>
+   
+  )
+}
+
+export default App
